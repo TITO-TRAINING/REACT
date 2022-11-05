b66197580c40730feb26fae15fb567d699123a70
bao-zb <bao.nguyen@zenbusiness.com>
2022-11-05 16:05:14 +0700

    test

--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Button from '../../components/Button';
 
 function Homepage() {
-  return <div>Homepage</div>;
+  const onClick = () => {
+    console.log('clicked'); // TODO: implement later
+  };
+  return (
+    <div>
+      <Link to="/about">Redirect About page</Link>
+      <Button
+        className="abvasda"
+        text="button"
+        onClick={onClick}
+        image="asdasd"
+      />
+      <Button text="button" onClick={onClick} />
+    </div>
+  );
 }
 
 export default Homepage;
